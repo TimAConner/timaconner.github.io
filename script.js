2d9a6047049bf04ab771893d53a1038e5d9ccfd1
Tim A. Conner <timaconner1@gmail.com>
2017-11-21 15:07:12 -0600

    Add blog.html and styling.

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var navHTML = '<li><a href="index.html">Resume</a></li><li><a href="contact.html">Contact</a></li><li><a href="aboutme.html">About Me</a></li><li><a href="projects.html">Projects</a></li>';
+var navHTML = '<li><a href="index.html">Resume</a></li><li><a href="contact.html">Contact</a></li><li><a href="aboutme.html">About Me</a></li><li><a href="projects.html">Projects</a></li><li><a href="blog.html">Blog</a></li>';
 
 function createMenu(){
     document.getElementById("menu").innerHTML = navHTML;

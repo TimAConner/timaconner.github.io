735bab40571d7cd5165b7b08afcbca7edc518d06
Tim A. Conner <timaconner1@gmail.com>
2017-12-21 17:18:55 -0500

    Added home to menu controller.

--- js/menuController.js
+++ js/menuController.js
@@ -2,14 +2,27 @@
 const model = require('./model');
 const view = require('./view');
 
+const pageControllers = {
+    "Blog" : require("./blogController"),
+    "Home" : require("./homeController")
+    
+};
 
-let navHTML = '<li><a href="index.html">Home</a></li><li><a href="resume.html">Resume</a></li><li><a href="contact.html">Contact</a></li><li><a href="aboutme.html">About Me</a></li><li><a href="projects.html">Projects</a></li><li><a href="blog.html">Blog</a></li>';
+// let navHTML = '<li><a href="index.html">Home</a></li><li><a href="resume.html">Resume</a></li><li><a href="contact.html">Contact</a></li><li><a href="aboutme.html">About Me</a></li><li><a href="projects.html">Projects</a></li><li><a href="blog.html">Blog</a></li>';
 
+const clearPage = () => {
+    document.getElementById("main-output").innerHTML = "";
+};
 
 const activateMenu = () => {
     let menu = document.getElementById("menu");
     menu.addEventListener("click", (event) => {
-        console.log(event.target);
+        clearPage();
+        
+        let title = event.target.id;
+        document.getElementById("page-title").innerText = title;
+        document.title = title;
+        pageControllers[title][`build${title}`]();
     });
 };
 

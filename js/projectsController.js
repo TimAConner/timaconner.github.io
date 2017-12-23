9db80ca97ae5a1d0d3a6dc1361496c5205bb04eb
Tim A. Conner <timaconner1@gmail.com>
2017-12-23 14:05:45 -0500

    Added project searching.

--- js/projectsController.js
+++ js/projectsController.js
@@ -3,8 +3,32 @@
 const model = require('./model');
 const view = require('./view');
 
+let allProjects = [];
+let selectedProjects = [];
+
 module.exports.buildProjects = () => {
     model.fetchData("../json/projects.json").then(projectsObject => {
-        view.showProjects(projectsObject.projects);
+        allProjects = projectsObject.projects;
+        selectedProjects = allProjects;
+        view.showProjects(selectedProjects);
+        activateSearchDiv();
     });
 };  
+
+const activateSearchDiv = () => {
+    let searchBox = document.getElementById("search-box");
+    searchBox.addEventListener("keyup", (e) => {
+        let term = searchBox.value.toLowerCase();
+
+        if(term.length >= 3){
+            selectedProjects = allProjects.filter((project) => (project.name.toLowerCase().indexOf(term) !== -1 || project.description.toLowerCase().indexOf(term) !== -1));
+            view.clearMainOutput();
+            view.showProjects(selectedProjects);
+        } else {
+            view.clearMainOutput();
+            selectedProjects = allProjects;
+            view.showProjects(selectedProjects);
+        }
+
+    });
+};

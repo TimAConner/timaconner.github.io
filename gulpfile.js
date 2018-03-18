cd6188c3e694f3effd9ca0ae7e473bd2dda2366d
Tim A. Conner <timaconner1@gmail.com>
2018-03-17 22:21:17 -0500

    Add css concat to gulp and css reset.

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,6 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var concatCss = require('gulp-concat-css');
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
@@ -10,7 +11,13 @@ gulp.task('fonts', function() {
   return gulp.src('node_modules/font-awesome/css/*')
     .pipe(gulp.dest('app/css'))
 })
+gulp.task('compile-css', function () {
+  return gulp.src('app/css/**/*.css')
+    .pipe(concatCss('bundle.css'))
+    .pipe(gulp.dest('app/cssBundle/'));
+});
 
 gulp.task('watch', function(){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
+  gulp.watch('app/css/**/*.css', ['compile-css']); 
 })

c0cb099e70f9019304c1cdd73bf0677ef6920277
Tim A. Conner <timaconner1@gmail.com>
2018-03-17 22:34:46 -0500

    Add font awesome.

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,26 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var concatCss = require('gulp-concat-css');
+var fontAwesome = require('node-font-awesome');
+
+
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) // Using gulp-sass
     .pipe(gulp.dest('app/css'))
 });
-gulp.task('fonts', function() {
-  return gulp.src('node_modules/font-awesome/css/*')
-    .pipe(gulp.dest('app/css'))
-})
 gulp.task('compile-css', function () {
   return gulp.src('app/css/**/*.css')
     .pipe(concatCss('bundle.css'))
     .pipe(gulp.dest('app/cssBundle/'));
 });
+gulp.task('fonts', function() {
+  gulp.src(fontAwesome.fonts)
+    .pipe(gulp.dest('./app/fonts'));
+ });
 
 gulp.task('watch', function(){
   gulp.watch('app/scss/**/*.scss', ['sass']); 
-  gulp.watch('app/css/**/*.css', ['compile-css']); 
+  gulp.watch('app/css/**/*.css', ['fonts', 'compile-css']); 
 })

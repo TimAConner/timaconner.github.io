529550baf677e6a4ff5fe4205e9865cfbf3a1ea9
Tim A. Conner <timaconner1@gmail.com>
2018-03-23 19:11:13 -0500

    Update Gulp to continue running after an error.

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,30 @@
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var concatCss = require('gulp-concat-css');
-var fontAwesome = require('node-font-awesome');
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const concatCss = require('gulp-concat-css');
+// var fontAwesome = require('node-font-awesome');
 
-
+function swallowError (error) {
+  console.log(error.toString())
+  this.emit('end')
+}
 
 gulp.task('sass', function(){
   return gulp.src('app/scss/**/*.scss')
     .pipe(sass()) // Using gulp-sass
+    .on('error', swallowError)
     .pipe(gulp.dest('app/css'))
 });
 
 gulp.task('compile-css', function () {
   return gulp.src('app/css/**/*.css')
     .pipe(concatCss('bundle.css'))
+    .on('error', swallowError)
     .pipe(gulp.dest('app/cssBundle/'));
 });
 
-gulp.task('fonts', function() {
-  gulp.src(fontAwesome.fonts)
-    .pipe(gulp.dest('./app/fonts'));
- });
-
 gulp.task('watch', function(){
+  gulp.start('sass');
+  gulp.start('compile-css');
   gulp.watch('app/scss/**/*.scss', ['sass']); 
-  gulp.watch('app/css/**/*.css', ['fonts', 'compile-css']); 
+  gulp.watch('app/css/**/*.css', ['compile-css']); 
 })

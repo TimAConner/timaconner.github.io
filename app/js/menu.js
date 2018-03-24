1da595f80d150a26dd8d9bafc8f4b448aec5d9bb
Tim A. Conner <timaconner1@gmail.com>
2018-03-23 19:00:17 -0500

    Style menu and add scrolling functionality.

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,10 +1,37 @@
 'use strict';
 // Activates the mobile menu button
 (() => {
-    const mobileMenuBurger = document.getElementById('mobile-menu-burger');
-    const menu = document.getElementById('menu');
-    mobileMenuBurger.addEventListener('click', () => {
+    const mobileMenuBurger = document.querySelector('#mobile-menu-burger');
+    const menu = document.querySelector('#menu');
+    const menuButtons = document.querySelectorAll('.menu-button');
+
+    const toggleMobileMenu = () => {
+        isMobileMenuOpen = !isMobileMenuOpen;
         mobileMenuBurger.classList.toggle('x-burger');
         menu.classList.toggle('pull-out-mobile-menu');
+    };
+
+    let isMobileMenuOpen = false;
+
+    mobileMenuBurger.addEventListener('click', () => {
+        toggleMobileMenu();
     });
+
+    [...menuButtons].forEach(button => {
+        button.addEventListener('click', function (event) {
+            // Remove # from beginning of string
+            const targetDiv = event.target.getAttribute("href").substring(1);
+            if (targetDiv !== null) {
+                document.getElementById(targetDiv).scrollIntoView();
+            }
+        });
+    });
+
+    // A click not on menu burger
+    document.querySelectorAll('body')[0].addEventListener('click', function (event) {
+        if (isMobileMenuOpen && !event.target.classList.contains('mobileMenuBurgerButton')) {
+            toggleMobileMenu();
+        }
+    });
+
 })();
